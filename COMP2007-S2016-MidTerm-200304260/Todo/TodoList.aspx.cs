17ffbf88fedc95e83541f22d9e329777d5d763b3
Bloodham <bloodham25@gmail.com>
2016-06-23 18:52:04 -0400

    Comments/TodoList/TodoDetails

--- COMP2007-S2016-MidTerm-200304260/Todo/TodoList.aspx.cs
+++ COMP2007-S2016-MidTerm-200304260/Todo/TodoList.aspx.cs
@@ -5,6 +5,11 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+using COMP2007_S2016_MidTerm_200304260.Models;
+using System.Web.ModelBinding;
+using System.Linq.Dynamic;
+
+
 namespace COMP2007_S2016_MidTerm_200304260
 {
     public partial class TodoList : System.Web.UI.Page
@@ -12,7 +17,92 @@ namespace COMP2007_S2016_MidTerm_200304260
 
         protected void Page_Load(object sender, EventArgs e)
         {
+            
+            //if loading page for the first time, populate the grid
+            if (!IsPostBack)
+            {
+                //get the todo list data
+                this.GetTodo();
+            }
+        }
+        /*
+         * <summary>
+         * Grabs the todo from database and puts into a gridview
+         * </summary>
+         * @method GetTodo
+         * @return {void}
+         */
+        protected void GetTodo()
+        {
+            // connect to EF
+            //connect to EF
+            using (TodoConnection db = new TodoConnection())
+            {
+                //query the Games table using EF and LINQ
+                var Todos = (from allTodos in db.Todos
+                             select allTodos);
 
+                //bind results to gridview
+                TodoGridView.DataSource = Todos.AsQueryable().ToList();
+                TodoGridView.DataBind();
+            }
+        }
+
+        /*
+         * <summary>
+         * This allows the list to be paginated
+         * </summary>
+         * @method TodosGridView_PageIndexChanging
+         * @param {object} sender
+         * @param {GridViewPageEventArgs} e
+         * @returns {void}
+         */
+
+        protected void TodoGridView_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            //store which row was clicked
+            int selectedRow = e.RowIndex;
+
+            //get the selected todoid using the grids datakey collection
+            int TodoID = Convert.ToInt32(TodoGridView.DataKeys[selectedRow].Values["TodoID"]);
+
+            //use ef to find the selected todo and delete it
+            using (TodoConnection db = new TodoConnection())
+            {
+                //create object of the todo class and store the query string inside of it
+                Todo deletedTodo = (from todoRecords in db.Todos
+                                    where todoRecords.TodoID == TodoID
+                                    select todoRecords).FirstOrDefault();
+
+                //remove the selected todo from the db
+                db.Todos.Remove(deletedTodo);
+
+                //save db changes
+                db.SaveChanges();
+
+                //refresh gridview
+                this.GetTodo();
+
+            }
+        }
+
+        /*
+        * <summary>
+        * Allows multiple pages to be viewed at once
+        * </summary>
+        * @method PageSizeDropDownList_SelectedIndexChanged
+        * @param {object} sender
+        * @param {GridViewPageEventArgs} e
+        * @returns {void}
+        */
+
+        protected void PageSizeDropDownList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //set the new page size
+            TodoGridView.PageSize = Convert.ToInt32(PageSizeDropDownList.SelectedValue);
+
+            //refresh
+            this.GetTodo();
         }
 
     }
